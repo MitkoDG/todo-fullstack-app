a34552288876679e51846c0eb3828057e7ce2859
MitkoDG <retimid@abv.bg>
2023-05-02 22:34:33 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import ListHeaders from './components/ListHeaders';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ListHeaders listName={'Priority List'} />
+    </div>
+  );
+}
+
+export default App;
