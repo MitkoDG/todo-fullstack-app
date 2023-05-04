d740b70ad5c207f772a0ce4dcf8031e6af526309
MitkoDG <retimid@abv.bg>
2023-05-04 22:51:10 +0300

    connect to the db

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require('pg').Pool
 require('dotenv').config();
 
 const pool = new Pool({
-    user: 'ddg',
-    password: 'ddg',
-    host: 'localhost',
-    port: 5432,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DBPORT,
     database: 'tododb'
 });
 
--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const PORT = process.env.PORT || 8000
 const express = require('express');
 
 const app = express();
+const pool = require('./db');
 
-app.get('/', (req, res) => {
-    res.send('Hello, DDG');
+app.get('/', async (req, res) => {
+    const userEmail = 'ddg@dimitargegov.com'
+    try {
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
+        res.json(todos.rows);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
