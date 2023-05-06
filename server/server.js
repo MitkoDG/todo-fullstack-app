92179fd94100c8825f570f94706361f171afc799
MitkoDG <retimid@abv.bg>
2023-05-06 23:08:22 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(cors());
 
 app.get('/todos/:userEmail', async (req, res) => {
     const { userEmail } = req.params;
+    console.log(userEmail);
     try {
         const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]);
         res.json(todos.rows);
