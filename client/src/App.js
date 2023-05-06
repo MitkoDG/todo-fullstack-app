71665c1bec6fcf0afa72e3c0007ead62f1b9a96f
MitkoDG <retimid@abv.bg>
2023-05-06 16:44:42 +0300

    fetching data from the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,25 @@
 import ListHeaders from './components/ListHeaders';
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const userEmail = 'dimitargegov@gmail.com';
+  const [ tasks, setTasks ] = useState(null);
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const data = await response.json();
+      setTasks(data);
+    } catch (err) {
+      console.error(err);
+    }
+
+  }
+
+  useEffect(() => getData, []);
+
+  console.log(tasks);
+
+
   return (
     <div className="app">
       <ListHeaders listName={'Priority List'} />
