bc3b4663ceb0d0948e88ec98171d762f00a3588f
MitkoDG <retimid@abv.bg>
2023-05-14 20:30:32 +0300

    added DELETE todos on client site

--- client/src/components/ListItems.js
+++ client/src/components/ListItems.js
@@ -5,7 +5,22 @@ import TickIcon from "./TickIcon";
 
 const ListItems = ({ task, getData }) => {
     const [showModal, setShowModal] = useState(false);
-    
+
+    const deleteItem = async () => {
+        try {
+            const response = await fetch(`http://127.0.0.1:8000/todos/${task.id}`, {
+                method: "DELETE",
+                // headers: {
+                //     "Content-Type": "application/json",
+                // },
+            })
+            if (response.status === 200) {
+                getData();
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    }
     return (
         <li className="list-item">
 
@@ -17,7 +32,7 @@ const ListItems = ({ task, getData }) => {
 
             <div className="list-header-buttons">
                 <button className="edit" onClick={() => setShowModal(true)}>Edit</button>
-                <button className="delete">Delete</button>
+                <button className="delete" onClick={deleteItem}>Delete</button>
             </div>
             {showModal && <Modal mode={'edit'} setShowModal={setShowModal} task={task} getData={getData} />}
 
