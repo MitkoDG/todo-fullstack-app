4113113d46d0235b281bcfec71416c33bfbe15e6
MitkoDG <retimid@abv.bg>
2023-05-21 18:39:13 +0300

    added server login res

--- server/server.js
+++ server/server.js
@@ -62,16 +62,42 @@ app.post('/signup', async (req, res) => {
 
     try {
         const signUp = await pool.query(`INSERT INTO users (email, hashed_password) VALUES($1, $2)`,
-        [email, hashedPassword]);
+            [email, hashedPassword]);
 
-        const token = jwt.sign({ email }, 'secret', {expiresIn: '1hr'});
+        const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' });
 
-        res.json({email, token});
+        res.json({ email, token });
     } catch (err) {
         console.log(err.detail);
         console.error(err + 'error hashing password');
 
     }
 });
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const users = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+
+        if (!users.rows.length) return res.json({ detail: 'User does not exist' });
+
+        const success = await bcrypt.compare(password, users.rows[0].hashed_password);
+
+        const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' });
+        if (success) {
+            res.json({ 'email' : users.rows[0].email, token });
+        } else {
+            res.json({ detail: "Login failed"})
+        }
+        
+    } catch (err) {
+        console.log(err.detail);
+        console.error(err + 'error hashing password');
+
+    }
+});
+
+
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
